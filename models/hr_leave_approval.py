e2fb3d4bc83dc90d4137f3d715914b9daafa41ef
root <root@PAJ-DH>
2025-02-24 00:06:40 +0700

    revise reviewer

--- models/hr_leave_approval.py
+++ models/hr_leave_approval.py
@@ -4,6 +4,6 @@ class HrLeaveApproval(models.Model):
     _name = 'hr.leave.approval'
     _description = 'HR Leave Approval'
 
-    approval_manager_id = fields.Many2one('res.users', string='Approval Manager')
-    approval_head_id = fields.Many2one('res.users', string='Approval Head')
+    approval_manager_id = fields.Many2one('res.users', string='Reviewer')
+    approval_head_id = fields.Many2one('res.users', string='Approver')
     employee_id = fields.Many2one('hr.employee', string='Employee')
--- models/hr_leave_settings.py
+++ models/hr_leave_settings.py
@@ -11,13 +11,3 @@ class HrLeaveSettings(models.Model):
         ('lead', 'Lead')
     ], string='Rank', required=True)
     allowable_days = fields.Integer(string='Allowable Leave Days')
-
-
-    @api.depends('rank')
-    def _compute_allowable_days(self):
-        for record in self:
-            if record.rank:
-                setting = self.env['hr.leave.settings'].search([('rank', '=', record.rank)], limit=1)
-                record.allowable_days = setting.allowable_days if setting else 0
-
-
