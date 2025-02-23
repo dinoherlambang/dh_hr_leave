f15e65a01e6335223282534804e60cbd8ab57b21
root <root@PAJ-DH>
2025-02-24 01:10:48 +0700

    revise on review state

--- models/hr_leave.py
+++ models/hr_leave.py
@@ -44,9 +44,27 @@ class HrLeave(models.Model):
                 })
         return res
 
+    # def action_confirm(self):
+    #     self.write({'state': 'confirm'})
+    #     self._create_activity_for_manager()
+
     def action_confirm(self):
         self.write({'state': 'confirm'})
-        self._create_activity_for_manager()
+        # Clear any existing activities
+        self.activity_ids.unlink()
+        # Create activity only for manager review
+        if self.approval_id and self.approval_id.approval_manager_id:
+            model_id = self.env['ir.model']._get('hr.leave').id
+            self.env['mail.activity'].create({
+                'res_model_id': model_id,
+                'res_model': 'hr.leave',
+                'res_id': self.id,
+                'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
+                'summary': 'Leave Request needs Review',
+                'note': 'A leave request from %s to %s has been confirmed.' % (self.start_date, self.end_date),
+                'user_id': self.approval_id.approval_manager_id.id,
+                'date_deadline': fields.Date.today(),
+            })
 
     def action_approve(self):
         self._check_allowable_days()
@@ -59,9 +77,27 @@ class HrLeave(models.Model):
     def action_cancel(self):
         self.write({'state': 'cancel'})
 
+    # def action_in_review(self):
+    #     self.write({'state': 'in_review'})
+    #     self._create_activity_for_head()
+
     def action_in_review(self):
         self.write({'state': 'in_review'})
-        self._create_activity_for_head()
+        # Clear any existing activities
+        self.activity_ids.unlink()
+        # Create activity only for head approval
+        if self.approval_id and self.approval_id.approval_head_id:
+            model_id = self.env['ir.model']._get('hr.leave').id
+            self.env['mail.activity'].create({
+                'res_model_id': model_id,
+                'res_model': 'hr.leave',
+                'res_id': self.id,
+                'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
+                'summary': 'Leave Request needs Approval',
+                'note': 'A leave request from %s to %s is in review.' % (self.start_date, self.end_date),
+                'user_id': self.approval_id.approval_head_id.id,
+                'date_deadline': fields.Date.today(),
+            })
 
     def action_draft(self):
         self.write({'state': 'draft'})
@@ -71,31 +107,31 @@ class HrLeave(models.Model):
             template = self.env.ref('dh_hr_leave.leave_approval_notification')
             self.env['mail.template'].browse(template.id).send_mail(leave.id, force_send=True)
 
-    def _create_activity_for_manager(self):
-        for leave in self:
-            if leave.approval_id and leave.approval_id.approval_manager_id:
-                self.env['mail.activity'].create({
-                    'res_model': 'hr.leave',
-                    'res_id': leave.id,
-                    'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
-                    'summary': 'Leave Request needs Review',
-                    'note': 'A leave request from %s to %s has been confirmed.' % (leave.start_date, leave.end_date),
-                    'user_id': leave.approval_id.approval_manager_id.id,
-                    'date_deadline': fields.Date.today(),
-                })
+    # def _create_activity_for_manager(self):
+    #     for leave in self:
+    #         if leave.approval_id and leave.approval_id.approval_manager_id:
+    #             self.env['mail.activity'].create({
+    #                 'res_model': 'hr.leave',
+    #                 'res_id': leave.id,
+    #                 'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
+    #                 'summary': 'Leave Request needs Review',
+    #                 'note': 'A leave request from %s to %s has been confirmed.' % (leave.start_date, leave.end_date),
+    #                 'user_id': leave.approval_id.approval_manager_id.id,
+    #                 'date_deadline': fields.Date.today(),
+    #             })
 
-    def _create_activity_for_head(self):
-        for leave in self:
-            if leave.approval_id and leave.approval_id.approval_head_id:
-                self.env['mail.activity'].create({
-                    'res_model': 'hr.leave',
-                    'res_id': leave.id,
-                    'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
-                    'summary': 'Leave Request needs Approval',
-                    'note': 'A leave request from %s to %s is in review.' % (leave.start_date, leave.end_date),
-                    'user_id': leave.approval_id.approval_head_id.id,
-                    'date_deadline': fields.Date.today(),
-                })
+    # def _create_activity_for_head(self):
+    #     for leave in self:
+    #         if leave.approval_id and leave.approval_id.approval_head_id:
+    #             self.env['mail.activity'].create({
+    #                 'res_model': 'hr.leave',
+    #                 'res_id': leave.id,
+    #                 'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
+    #                 'summary': 'Leave Request needs Approval',
+    #                 'note': 'A leave request from %s to %s is in review.' % (leave.start_date, leave.end_date),
+    #                 'user_id': leave.approval_id.approval_head_id.id,
+    #                 'date_deadline': fields.Date.today(),
+    #             })
 
     def _check_allowable_days(self):
         leave_days = (self.end_date - self.start_date).days + 1
